1e645314c9c6bfe17493abe9a64fff7cb87c50d3
charques <carloshernandezarques@gmail.com>
2017-03-23 08:01:14 +0100

    first approach replace

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TLP-Haskell-KnightTravel
 Prácticas Haskell - UNED Teoría de los Lenguajes de Programación 2016-2017
+
+Iniciar el GHCI y carga del módulo. 
+
+```haskell
+ghci
+:load knight-travel
+```
+Recargar los módulos cargados previamente.
+```haskell
+:reload
+```
---
+++ knightTravel.hs
@@ -0,0 +1,63 @@
+module Knight where
+
+-- Data Types used in program
+
+type File = [Bool]
+type Board = [File]
+type Square = (Int,Int)
+type Path = [Square]
+
+-- Backtracking scheme
+
+bt :: (a -> Bool) -> (a -> [a]) -> a -> [a]
+bt isSol comp node
+  | isSol node = [node]
+  | otherwise  = concat (map (bt isSol comp) (comp node))
+-- bt lo está aplicacion parcialmente (solo 2 params). Devuelve una funcion que espera un nodo (tercer parámetro)
+
+
+-- Auxiliary Functions
+
+--fullPath :: Node -> Bool
+--fullPath node = false
+-- reduce o fall left
+-- Se trata de una función que comprueba si un nodo contiene, o no, un camino completo (que recorra todo el tablero).
+
+--validJumps :: Node -> [Node]
+--validJumps node =
+-- Esta función recibe un nodo y devuelve la lista de compleciones de dicho nodo, esto es, todos aquellos nodos en los
+-- que se visita una nueva casilla válida (que está dentro del tablero y que aún no ha sido visitada).
+
+createFile :: Int -> File
+createFile size = [False | _ <- [1..size]]
+
+createBoard :: Int -> Board
+createBoard size = [createFile size | _ <- [1..size]]
+
+activateSquare :: Board -> Square -> File
+activateSquare board (x,y) =
+    let file_to_modify = board !! x
+        --modified_file = updateFile file_to_modify y
+    in  updateFile file_to_modify y
+
+updateFile :: File -> Int -> File
+updateFile  (f:fs) index
+    | index == 0     = not f:fs
+    | otherwise  = f : (updateFile fs (index-1))
+updateFile [] index = []
+
+-- Main Functions
+
+type Node = (Int,Int,Board,Square,Path)
+
+--knightTravel :: Int -> Square -> Path
+--knightTravel size square =
+--    let board = createBoard size
+--        path = []
+--        node = (size, size, board, square, path)
+--    in bt fullPath validJumps node
+
+testActivate :: Int -> File
+testActivate size =
+    let board = createBoard size
+    in activateSquare board (0,1)
